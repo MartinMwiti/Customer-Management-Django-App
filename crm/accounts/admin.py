43c1b6d99af9f38ff9ddce26fedf1e18e1b79c67
MartinMwiti <martinmwiti007@gmail.com>
2020-05-03 14:32:26 +0300

    database models

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import * # IMPORT ALL MODELS
+
+admin.site.register(Customer) # to view the created customer database table in the admin
+admin.site.register(Product)
+admin.site.register(Order)
