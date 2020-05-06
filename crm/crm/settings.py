64750bb82dec07874bf9c3158248534f00051a06
MartinMwiti <martinmwiti007@gmail.com>
2020-05-06 22:03:36 +0300

    password reset

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -133,3 +133,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# sending email for password reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
